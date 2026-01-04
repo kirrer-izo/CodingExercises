ed5981d1f5951db843ef72bff338fc20d3a3a4ae
Isaack Muchoki <isaackmuchoki55@gmail.com>
2026-01-04 10:51:15 +0300

    created class FlourPacker with method named canPac with three int parameters

---
+++ src/FlourPacker.java
@@ -0,0 +1,38 @@
+public class FlourPacker {
+
+    public static void main(String[] args) {
+
+        System.out.println(canPack(1, 0, 5));
+        System.out.println(canPack(0, 5, 4));
+        System.out.println(canPack(2, 2, 11));
+        System.out.println(canPack(-3, 2, 12));
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        boolean result = false;
+        int totalBigWeight = bigCount * 5;
+
+        if (totalBigWeight >= goal) {
+            int remainder = goal % 5;
+
+            if (smallCount >= remainder) {
+                result = true;
+            }
+        }
+
+        if (totalBigWeight < goal) {
+            int remainingWeight = goal - totalBigWeight;
+            if (remainingWeight > 0 && smallCount >= remainingWeight) {
+                result = true;
+            }
+        }
+
+        return result;
+    }
+}
